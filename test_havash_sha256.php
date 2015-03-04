eb1f183b00e2cb91c8fc94f355febc95a24b7fdc
abderraouf-adjal <abderraouf.adjal@gmail.com>
2015-03-04 10:55:07 +0100

    havash 0.2.0

--- test_havash_sha256.php
+++ test_havash_sha256.php
@@ -2,19 +2,20 @@
 
 require_once('havash.php');
 
+$test_password = 'GPp7jd6zJ2QJBJR8';
 
-/* bool havash_hash($passwd, $hash_func = 'sha256', $repeats = 2000) */
 /* Make the hash and the salt (return array[Hash hex, Salt]) */
-$hash_and_salt = havash_hash('GPp7jd6zJ2QJBJR8');
-
+$hash_and_salt = havash_hash($test_password);
+if ($hash_and_salt < 0) {
+	goto fail;
+}
 
 $passwd_hash = $hash_and_salt[0];
 $salt = $hash_and_salt[1];
 
 
 /* Check the password */
-/* bool havash_check($passwd, $passwd_hash, $salt, $hash_func = 'sha256', $repeats = 2000) */
-$havash_check_return = havash_check('GPp7jd6zJ2QJBJR8', $passwd_hash, $salt);
+$havash_check_return = havash_check($test_password, $passwd_hash, $salt);
 
 if ($havash_check_return === true) {
 	echo 'Same hash'.PHP_EOL;
@@ -24,12 +25,14 @@ else if ($havash_check_return === false) {
 }
 else {
 	/* havash_check() Fail */
-	exit(1);
+	goto fail;
 }
 
 
 echo 'The Hash: '.$passwd_hash.PHP_EOL;
 echo 'The Salt: '.$salt.PHP_EOL;
 
+fail:
+	exit(1);
 
 ?>
